bd0cf8a7c7b051b43bc4894f2f3b109b90f735d7
phuong <huynhphuong04092006@gmail.com>
2026-04-01 10:01:42 +0700

    Create a project structure

---
+++ js/lib.js
@@ -0,0 +1,10 @@
+﻿<!DOCTYPE html>
+<html lang = "vi" >
+     <head>
+		<title>thiết kế trang web</title>
+		<meta charet = "utf-8"
+     </head>
+	<body>
+		<P>tiếp tục thiết kế trang</P>
+	</body>
+</html>
